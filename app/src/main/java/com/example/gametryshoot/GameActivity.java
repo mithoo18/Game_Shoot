8125818f5296b34f639cca266619ced50db0d9c7
Devanshu Gupta <devanshu.gupta@student.amity.edu>
2020-10-08 00:15:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gametryshoot/GameActivity.java
@@ -0,0 +1,34 @@
+package com.example.gametryshoot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class GameActivity extends AppCompatActivity {
+    private GameView gameview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Point point = new Point();
+        getWindowManager().getDefaultDisplay().getSize(point);
+        gameview = new GameView(this, point.x, point.y);
+        setContentView(gameview);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gameview.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gameview.resume();
+    }
+}
