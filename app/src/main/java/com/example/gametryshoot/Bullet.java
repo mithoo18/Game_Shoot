d794ff267af90e6f56f9899f1f17e14f1482679a
mits <devanshu18g@gmail.com>
2021-05-28 15:00:44 +0530

    Tested & Images

---
+++ app/src/main/java/com/example/gametryshoot/Bullet.java
@@ -0,0 +1,33 @@
+package com.example.gametryshoot;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+import static com.example.gametryshoot.GameView.screenRatioX;
+import static com.example.gametryshoot.GameView.screenRatioY;
+
+public class Bullet {
+
+    int x, y, width, height;
+    Bitmap bullet;
+
+    public Bullet(Resources resources) {
+        bullet = BitmapFactory.decodeResource(resources, R.drawable.bullet);
+
+        width = bullet.getWidth();
+        height = bullet.getHeight();
+
+        width /= 4;
+        height /= 4;
+
+        width = (int) (width *screenRatioX);
+        height = (int) (height *screenRatioY);
+
+        bullet = Bitmap.createScaledBitmap(bullet, width, height,false);
+
+    }
+
+    Rect getCollisionShape() {
+        return new Rect(x, y, x + width, y + height);
+    }
+}
